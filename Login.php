c524782e3b65fde249aba33cb215e45edca3655d
WackoJacko10 <jackanthony14@hotmail.com>
2022-04-14 19:53:06 +0100

    log in, log out and main page with style sheet  JA
    log in, log out and main page with style sheet
    JA

---
+++ Login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Sentinel Login</title>
+</head>
+<body>
+
+<div action="action_page.php" method="post">
+
+        <img src="coop-logo.png" alt="Avatar" class="avatar">
+
+
+<div class="fixed">
+</div>
+
+    <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <button type="submit">Login</button>
+    </div>
+
+    <div class="forgot">
+        <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+
+</div>
+</form>
+</body>
+</html>
